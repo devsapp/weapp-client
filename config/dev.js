b5a80adb954606b16ac9de5e345e3139ce559403
IOLOII <yuey@coinioc.cc>
2022-11-03 17:55:48 +0800

    🧪

--- config/dev.js
+++ config/dev.js
@@ -5,11 +5,12 @@ module.exports = {
   },
   defineConstants: {
     API_HOST:
-      '"http://localhost:9000"',
+      // '"http://localhost:9000"',
+      '"https://server-weapp-nxhrqmqttk.cn-shenzhen.fcapp.run"',
   },
   mini: {},
   h5: {},
   plugins: [
-    '@tarojs/plugin-react-devtools'
+    // '@tarojs/plugin-react-devtools'
   ]
 };
--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,23 +1,63 @@
-import { Component } from 'react';
-import { View, Button } from '@tarojs/components';
-import styles from './index.module.less';
-import { Block, UserAvatar } from '@/components/shared';
-import CachedData from '@/cache/common';
-import Taro from '@tarojs/taro';
-import OpenId from '@/components/shared/openid';
+import { Component } from 'react'
+import { View, Button } from '@tarojs/components'
+import styles from './index.module.less'
+import { Block, UserAvatar } from '@/components/shared'
+import CachedData from '@/cache/common'
+import Taro from '@tarojs/taro'
+import OpenId from '@/components/shared/openid'
+import C from '@/constant/config'
 
 export default class Index extends Component {
-  componentWillMount() {}
+  componentWillMount() { }
 
-  componentDidMount() {}
+  componentDidMount() { }
 
-  componentWillUnmount() {}
+  componentWillUnmount() { }
 
-  componentDidShow() {}
+  componentDidShow() { }
 
-  componentDidHide() {}
+  componentDidHide() { }
 
   render() {
+
+    const onClick = () => {
+      Taro.login({
+        success: (res) => {
+          if (res.code) {
+            Taro.request({
+              url: `http://localhost:9000/api/openid`,
+              header: {
+                token: res.code,
+                env: Taro.getEnv(),
+              },
+              data: {
+              },
+              success: (res) => {
+                console.log("local api res :",res.data)
+                const { error, data, ErrorCode } = res.data
+                let errorMessage = error || ErrorCode
+                if (errorMessage) {
+                  Taro.showToast({
+                    title: errorMessage,
+                    duration: 2000,
+                    icon: 'error',
+                    mask: false,
+                  })
+                } else {
+                  if (data.length) {
+                    console.log(data[0])
+                  }
+                }
+              },
+              complete: () => {
+              },
+            })
+          }
+        },
+      })
+    }
+
+
     return (
       <View className={styles.create}>
         <UserAvatar />
@@ -27,12 +67,19 @@ export default class Index extends Component {
           className="btn-max-w mt-100"
           type="primary"
           onClick={() => {
-            Taro.navigateBack();
+            Taro.navigateBack()
           }}
         >
           返回
         </Button>
+        <Button
+          className="btn-max-w mt-100"
+          type="primary"
+          onClick={onClick}
+        >
+          请求
+        </Button>
       </View>
-    );
+    )
   }
 }
